826f5fa6aeb0588a97e604cc56b66b407ea02c00
Scott Ungchusri <animoogle504@gmail.com>
2018-07-12 06:20:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,9 @@ def key_for_min_value(name_hash)
   my_minimum = nil
   my_return = nil
     name_hash.each do |name, value|
-    if(my_minimum == nil)
+    if(my_minimum == nil) #first iteration sets a baseline assuming there is something there.
       my_minimum = value
+      my_return = name
     elsif(value < my_minimum)
       my_minimum = value
       my_return = name
